8c101714f085d9e2ab3d2468ba6e5ac2a7818bd3
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-03 06:54:18 +0100

    task 3 done

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,52 @@
+
+#!/usr/bin/env python3
+"""A LRUCache that inherits from
+BaseCaching and is a caching system:
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """A Least Recently Used (LRU) caching system"""
+
+    def __init__(self):
+        """Initialize the LRUCache"""
+        super().__init__()
+        self.used_keys = []
+
+    def put(self, key, item):
+        """Store an item in the cache
+
+        Args:
+            key: The key to store the item under
+            item: The item to be stored
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.used_keys:
+                self.used_keys.append(key)
+            else:
+                self.used_keys.append(
+                    self.used_keys.pop(
+                        self.used_keys.index(key)))
+            if len(self.used_keys) > BaseCaching.MAX_ITEMS:
+                discarded_key = self.used_keys.pop(0)
+                del self.cache_data[discarded_key]
+                print('DISCARD: {:s}'.format(discarded_key))
+
+    def get(self, key):
+        """Retrieve an item from the cache
+
+        Args:
+            key: The key of the item to retrieve
+
+        Returns:
+            The item associated with the key, or None if not found
+        """
+        if key is not None and key in self.cache_data:
+            self.used_keys.append(
+                self.used_keys.pop(
+                    self.used_keys.index(key)))
+            return self.cache_data.get(key)
+        return None
