80c186b1d258bb4f82ccf269303775aaadcf719f
Romaric250 <lonfonyuyromeo@gmail.com>
2024-02-06 21:30:25 +0100

    task 2 updated

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """config for babel
+
+    Returns:
+            _type_: _description_
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """_a simple route
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
